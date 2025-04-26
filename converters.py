e556d2ace38c1eb74a477bccffac11cf7fcab723
rr3333mmAA <nice.amirkhan@gmail.com>
2025-04-26 15:46:12 +0200

    Added: `to_xml()`, `to_markdown()` methods.

--- converters.py
+++ converters.py
@@ -14,6 +14,16 @@ class PandocDocxConverter:
         except subprocess.CalledProcessError as e:
             print(f"Error during conversion: {e}")
 
+    def to_markdown(self, output_file: str):
+        """
+        Convert the input DOCX file to Markdown using Pandoc.
+        """
+        try:
+            subprocess.run(['pandoc', '-f', 'docx', '-t', 'markdown', self.input_file, '-o', output_file], check=True)
+            print(f"Successfully converted {self.input_file} to {output_file}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error during conversion: {e}")
+
 
 class SofficeDocxConverter:
     def __init__(self, input_file: str):
@@ -26,5 +36,28 @@ class SofficeDocxConverter:
         try:
             subprocess.run(['soffice', '--headless', '--convert-to', 'html', self.input_file, '--outdir', output_file], check=True)
             print(f"Successfully converted {self.input_file} to {output_file}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error during conversion: {e}")
+
+    def to_xml(self, output_dir: str):
+        # Note: too big
+        """
+        Convert the input DOCX file to XML using LibreOffice (soffice).
+        """
+        try:
+            subprocess.run(['soffice', '--headless', '--convert-to', 'xml', self.input_file, '--outdir', output_dir], check=True)
+            print(f"Successfully converted {self.input_file} to {output_dir}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error during conversion: {e}")
+
+    def to_markdown(self, output_dir: str):
+        """
+        Convert the input DOCX file to Markdown using LibreOffice (soffice).
+        """
+        # Error: no export filter for ignore/output/sample.markdown found, aborting.
+        # Error: no export filter
+        try:
+            subprocess.run(['soffice', '--headless', '--convert-to', 'markdown', self.input_file, '--outdir', output_dir], check=True)
+            print(f"Successfully converted {self.input_file} to {output_dir}")
         except subprocess.CalledProcessError as e:
             print(f"Error during conversion: {e}")
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     pandoc_converter = PandocDocxConverter(input_file)
     soffice_converter = SofficeDocxConverter(input_file)
     
-    pandoc_converter.to_html(f"{output_dir}/output_pandoc.html")
-    soffice_converter.to_html(f"{output_dir}/output_soffice.html")
+    pandoc_converter.to_markdown(f"{output_dir}/output_pandoc.md")
+    soffice_converter.to_markdown(f"{output_dir}")
